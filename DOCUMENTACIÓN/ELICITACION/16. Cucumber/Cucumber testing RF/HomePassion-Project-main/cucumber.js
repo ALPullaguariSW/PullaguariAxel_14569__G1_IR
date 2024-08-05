b05ac884b39dadd25964acc7dcac3998c678cb99
BingBong593 <150882583+ALPullaguariSW@users.noreply.github.com>
2024-08-05 08:47:15 -0500

    Add features to git repo

--- DOCUMENTACIÓN/ELICITACION/16. Cucumber/Cucumber testing RF/HomePassion-Project-main/cucumber.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  default: {
-    require: ['features/step_definitions/**/*.js'],
-    format: ['json:cucumber_report.json'], // Configuración para generar el archivo JSON
-    snippets: true, // Habilitar los snippets
-  },
-};
--- DOCUMENTACIÓN/ELICITACION/16. Cucumber/Cucumber testing RF/HomePassion-Project-main/src/types/Task.ts
+++
@@ -1,7 +0,0 @@
-// types/Task.ts
-export interface Task {
-    _id: string;
-    title: string;
-    description: string;
-  }
-  
